699ea62fce7db5b73e274b7472d3f571660d5f37
Mohammad Olid Afzal <123568599+olid1803@users.noreply.github.com>
2026-02-09 16:17:23 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+let name ="Olid"
+let age = "28"
+let isLoggedIn = false
+
+// number => 2 to power 23
+// bigint
+// string => ""
+// boolean => true/false
+// null= stand alone value
+// undefined 
+// symbol unique
+
+
+console.log (typeof undefined); //output undefined
+console.log (typeof null); // output object
