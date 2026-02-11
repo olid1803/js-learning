f1a12882760233b51a4818107388aad968476edc
Mohammad Olid Afzal <123568599+olid1803@users.noreply.github.com>
2026-02-11 15:28:29 +0000

    operations

---
+++ 01_basic/operations.js
@@ -0,0 +1,12 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = "olid"
+let str2 = " Afzal"
+let str3 = str1+str2
+console.log (str3)
+ let gameCounter = 100
+ gameCounter++
+ console.log(gameCounter)
